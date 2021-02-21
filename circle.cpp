fcf4b8016c4b51a8e811a6405a4773e9a28bf6a7
Shaili Regmi <shailiregmi1@gmail.com>
2021-02-21 14:32:53 -0500

    started circle

--- circle.cpp
+++ circle.cpp
@@ -1,4 +1,29 @@
 #include "circle.h"
 
 // your code here
+class Circle
+{
+	public
+	{
+		Circle()
+		{
+			radius = 0f;
+		}
 
+		Circle(float input_radius)
+		{
+			radius = input_radius;
+		}
+
+		Circle(const Circle &obj)
+		{
+			radius = new float;
+			*radius = *obj.radius;
+		}
+	}
+
+	private
+	{
+		float radius;
+	}
+};
